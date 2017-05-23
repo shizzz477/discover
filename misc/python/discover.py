10e0bccaf7dfbcc19f58bf8dc40d21bd3400ee1a
shizzz477 <marcgurreri@gmail.com>
2017-05-24 03:16:01 +0900

    local updates

--- misc/python/discover.py
+++ misc/python/discover.py
@@ -30,221 +30,243 @@ coloryellow = '\033[01;33m{0}\033[00m'
 line = '======================================'
 user = getpass.getuser()
 
+__all__ = [
+	'banner'
+]
+
+
 ##############################################################################################################
 
 def banner():
-     print
-     print '______  ___ ______ ______  _____  _    _ ______  _____'
-     print '|     \  |  |____  |      |     |  \  /  |_____ |____/'
-     print '|_____/ _|_ _____| |_____ |_____|   \/   |_____ |    \_Kali'
-     print
-     print 'By Lee Baird'
-     print
-     print
+	print
+	print '______  ___ ______ ______  _____  _    _ ______  _____'
+	print '|     \  |  |____  |      |     |  \  /  |_____ |____/'
+	print '|_____/ _|_ _____| |_____ |_____|   \/   |_____ |    \_Kali'
+	print
+	print 'By Lee Baird'
+	print
+	print
+
 
 ##############################################################################################################
 
 def cidr():
-     print 'CIDR'
-     goodbye()
+	print 'CIDR'
+	goodbye()
+
 
 ##############################################################################################################
 
 def error():
-     print
-     print
-     print colorred.format(line)
-     print
-     print colorred.format(' *** Invalid choice or entry. *** ')
-     print
-     print colorred.format(line)
-     time.sleep(2)
-     main()
+	print
+	print
+	print colorred.format(line)
+	print
+	print colorred.format(' *** Invalid choice or entry. *** ')
+	print
+	print colorred.format(line)
+	time.sleep(2)
+	main()
+
 
 ##############################################################################################################
 
 def goodbye():
-     print
-     print
-     print 'Coming soon...'
-     print
-     print
-     sys.exit(0)
+	print
+	print
+	print 'Coming soon...'
+	print
+	print
+	sys.exit(0)
+
 
 ##############################################################################################################
 
 def lan():
-     print 'LAN'
-     goodbye()
+	print 'LAN'
+	goodbye()
+
 
 ##############################################################################################################
 
 def listener():
-     print 'Metasploit Listener'
-     goodbye()
+	print 'Metasploit Listener'
+	goodbye()
+
 
 ##############################################################################################################
 
 def lists():
-     print 'List'
-     goodbye()
+	print 'List'
+	goodbye()
+
 
 ##############################################################################################################
 
 def main():
-     while True:
-          choice = menu()
-          if choice == "1":
-               execfile('recon.py')
-          elif choice == "2":
-               pingsweep()
-          elif choice == "3":
-               single()
-          elif choice == "4":
-               lan()
-          elif choice == "5":
-               lists()
-          elif choice == "6":
-               cidr()
-          elif choice == "7":
-               execfile('multitabs.py')
-          elif choice == "8":
-               nikto()
-          elif choice == "9":
-               execfile('sslcheck.py')
-          elif choice == "10":
-               subprocess.call('/opt/scripts/crack-wifi.sh')
-          elif choice == "11":
-               listener()
-          elif choice == "12":
-               sys.exit(0)
-          elif choice == "99":
-               updates()
-          else:
-               error()
+	while True:
+		choice = menu()
+		if choice == "1":
+			execfile('recon.py')
+		elif choice == "2":
+			pingsweep()
+		elif choice == "3":
+			single()
+		elif choice == "4":
+			lan()
+		elif choice == "5":
+			lists()
+		elif choice == "6":
+			cidr()
+		elif choice == "7":
+			execfile('multitabs.py')
+		elif choice == "8":
+			nikto()
+		elif choice == "9":
+			execfile('sslcheck.py')
+		elif choice == "10":
+			subprocess.call('/opt/scripts/crack-wifi.sh')
+		elif choice == "11":
+			listener()
+		elif choice == "12":
+			sys.exit(0)
+		elif choice == "99":
+			updates()
+		else:
+			error()
+
 
 ##############################################################################################################
 
 def menu():
-     os.system('clear')
-     banner()
-     print colorblue.format('RECON')
-     print '1. Scrape'
-     print
-     print colorblue.format('DISCOVER')+' - Host discovery, port scanning, service enumeration and OS'
-     print 'identification using Nmap, Nmap scripts and Metasploit scanners.'
-     print '2. Ping Sweep'
-     print '3. Single IP, URL or Range'
-     print '4. Local Area Network'
-     print '5. List'
-     print '6. CIDR Notation'
-     print
-     print colorblue.format('WEB')
-     print '7. Open multiple tabs in Firefox'
-     print '8. Nikto'
-     print '9. SSL Check'
-     print
-     print colorblue.format('MISC')
-     print '10. Crack WiFi'
-     print '11. Start a Metasploit listener'
-     print '12. Exit'
-     print
-     return raw_input('Choice: ')
+	os.system('clear')
+	banner()
+	print colorblue.format('RECON')
+	print '1. Scrape'
+	print
+	print colorblue.format('DISCOVER') + ' - Host discovery, port scanning, service enumeration and OS'
+	print 'identification using Nmap, Nmap scripts and Metasploit scanners.'
+	print '2. Ping Sweep'
+	print '3. Single IP, URL or Range'
+	print '4. Local Area Network'
+	print '5. List'
+	print '6. CIDR Notation'
+	print
+	print colorblue.format('WEB')
+	print '7. Open multiple tabs in Firefox'
+	print '8. Nikto'
+	print '9. SSL Check'
+	print
+	print colorblue.format('MISC')
+	print '10. Crack WiFi'
+	print '11. Start a Metasploit listener'
+	print '12. Exit'
+	print
+	return raw_input('Choice: ')
+
 
 ##############################################################################################################
 
 def nikto():
-     print 'Nikto'
-     goodbye()
+	print 'Nikto'
+	goodbye()
+
 
 ##############################################################################################################
 
 def pingsweep():
-     print 'Pingsweep'
-     goodbye()
+	print 'Pingsweep'
+	goodbye()
+
 
 ##############################################################################################################
 
 def reinstall_nmap():
-     print 'Reinstall nmap.'
-     goodbye()
+	print 'Reinstall nmap.'
+	goodbye()
+
 
 ##############################################################################################################
 
 def runlocally():
-     if not sys.stdout.isatty():
-          print
-          print line
-          print
-          print 'This option must be run locally, in an X-Windows environment.'
-          time.sleep(2)
-          main()
+	if not sys.stdout.isatty():
+		print
+		print line
+		print
+		print 'This option must be run locally, in an X-Windows environment.'
+		time.sleep(2)
+		main()
+
 
 ##############################################################################################################
 
 def scanname():
-     typeofscan()
+	typeofscan()
 
-     name = raw_input('Name of scan: ')
+	name = raw_input('Name of scan: ')
 
-     # Check for no answer
-     if name == "":
-          error()
+	# Check for no answer
+	if name == "":
+		error()
 
-     os.makedirs('/'+user+'/'+name)
+	os.makedirs('/' + user + '/' + name)
+
 
 ##############################################################################################################
 
 def single():
-     print 'Single'
-     goodbye()
+	print 'Single'
+	goodbye()
+
 
 ##############################################################################################################
 
 def sslcheck():
-     print 'SSLcheck'
-     goodbye()
+	print 'SSLcheck'
+	goodbye()
+
 
 ##############################################################################################################
 
 def typeofscan():
-     colorblue.format('Type of scan:')
-     print
-     print '1. External'
-     print '2. Internal'
-     print '3. Previous menu'
-     print
-     choice = raw_input('Choice: ')
+	colorblue.format('Type of scan:')
+	print
+	print '1. External'
+	print '2. Internal'
+	print '3. Previous menu'
+	print
+	choice = raw_input('Choice: ')
 
-     if choice == "1":
-          print
-          coloryellow.format('[*] Setting source port to 53.')
-          sourceport = 53
-          print
-          print line
-          print
+	if choice == "1":
+		print
+		coloryellow.format('[*] Setting source port to 53.')
+		sourceport = 53
+		print
+		print line
+		print
 
-     if choice == "2":
-          print
-          coloryellow.format('[*] Setting source port to 88.')
-          sourceport = 88
-          print
-          print line
-          print
+	if choice == "2":
+		print
+		coloryellow.format('[*] Setting source port to 88.')
+		sourceport = 88
+		print
+		print line
+		print
 
-     if choice == "3":
-          main()
-     else:
-          error()
+	if choice == "3":
+		main()
+	else:
+		error()
+
 
 ##############################################################################################################
 
 def updates():
-     print 'Updates'
-     goodbye()
+	print 'Updates'
+	goodbye()
+
 
 ##############################################################################################################
 
 if __name__ == '__main__':
-     main()
-
+	main()
--- misc/python/multitabs.py
+++ misc/python/multitabs.py
@@ -2,6 +2,8 @@
 
 import os
 
+from discover import banner, runlocally, colorblue
+
 os.system('clear')
 banner()
 
@@ -16,79 +18,79 @@ print
 choice = raw_input('Choice: ')
 
 if choice == "1":
-     print
-     location = raw_input('Enter the location of your list: ')
+	print
+	location = raw_input('Enter the location of your list: ')
 
-     if os.path.isfile(location):
-          f = open(location,'r') # Setup a read connection directory
-          filedata = f.read() # Read the file
-          f.close() # Close the connection
-          filedata = filedata.split('\n') # Turn into a list
-          filedata = [x for x in filedata if not x == ""] # Ignore blank lines
+	if os.path.isfile(location):
+		f = open(location, 'r')  # Setup a read connection directory
+		filedata = f.read()  # Read the file
+		f.close()  # Close the connection
+		filedata = filedata.split('\n')  # Turn into a list
+		filedata = [x for x in filedata if not x == ""]  # Ignore blank lines
 
-          port = raw_input('Port: ')
+		port = raw_input('Port: ')
 
-          if port.isdigit():
-               if int(port) in range(1,65535):
-                    if port == "21":
-                         for i in filedata:
-                              webbrowser.open('ftp://'+i)
-                              time.sleep(1)
-                    elif port == "80":
-                         for i in filedata:
-                              webbrowser.open('http://'+i)
-                              time.sleep(1)
-                    elif port == "443":
-                         for i in filedata:
-                              webbrowser.open('https://'+i)
-                              time.sleep(1)
-                    else:
-                         for i in filedata:
-                              webbrowser.open('http://'+i+':'+port)
-                              time.sleep(1)
-               else:
-                    error()
-          else:
-               error()
-     else:
-          error()
+		if port.isdigit():
+			if int(port) in range(1, 65535):
+				if port == "21":
+					for i in filedata:
+						webbrowser.open('ftp://' + i)
+						time.sleep(1)
+				elif port == "80":
+					for i in filedata:
+						webbrowser.open('http://' + i)
+						time.sleep(1)
+				elif port == "443":
+					for i in filedata:
+						webbrowser.open('https://' + i)
+						time.sleep(1)
+				else:
+					for i in filedata:
+						webbrowser.open('http://' + i + ':' + port)
+						time.sleep(1)
+			else:
+				error()
+		else:
+			error()
+	else:
+		error()
 
 if choice == "2":
-     print
-     print line
-     print
-     print 'Usage: target.com or target-IP'
-     print
-     domain = raw_input('Domain: ')
+	print
+	print line
+	print
+	print 'Usage: target.com or target-IP'
+	print
+	domain = raw_input('Domain: ')
 
-     # Check for no answer
-     if domain == "":
-          error()
+	# Check for no answer
+	if domain == "":
+		error()
 
-     response = urllib2.urlopen('http://'+domain+'/robots.txt')
-     robots = response.read()
-     robots = robots.split('\n')
-     
-     for i in robots:
-          if 'Disallow' in i:
-               j = i.split(' ')
-               f = open(os.path.expanduser('~')+'/'+domain+'-robots.txt','a')
-               f.write(j[1]+'\n')
-               f.close()
-               webbrowser.open('http://'+domain+j[1])
-               time.sleep(1)
+	response = urllib2.urlopen('http://' + domain + '/robots.txt')
+	robots = response.read()
+	robots = robots.split('\n')
 
-     print
-     print line
-     print
-     print '***Scan complete.***'
-     print
-     print 'The new report is located at /'+user+'/'+domain+'-robots.txt'
-     print
-     print
-     sys.exit(0)
+	for i in robots:
+		if 'Disallow' in i:
+			j = i.split(' ')
+			f = open(os.path.expanduser('~') + '/' + domain + '-robots.txt', 'a')
+			f.write(j[1] + '\n')
+			f.close()
+			webbrowser.open('http://' + domain + j[1])
+			time.sleep(1)
+
+	print
+	print line
+	print
+	print '***Scan complete.***'
+	print
+	print 'The new report is located at /' + user + '/' + domain + '-robots.txt'
+	print
+	print
+	sys.exit(0)
 
 if choice == "3":
-     main()
+	main()
 else:
-     error()
+	error()
