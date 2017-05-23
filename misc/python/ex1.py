ecea0204804243388b9a4e1936535b255c904bec
shizzz477 <marcgurreri@gmail.com>
2017-05-24 03:16:01 +0900

    local updates

--- misc/python/ex1.py
+++ misc/python/ex1.py
@@ -8,17 +8,16 @@ os.system('clear')
 port = raw_input('\nEnter a valid port: ')
 
 if port == '':
-     print '\nYou did not enter anything.\n\n'
-     sys.exit(1)
+	print '\nYou did not enter anything.\n\n'
+	sys.exit(1)
 
 try:
-     val = int(port)
+	val = int(port)
 except ValueError:
-     print('\nThat is not an number.\n\n')
-     sys.exit(1)
+	print('\nThat is not an number.\n\n')
+	sys.exit(1)
 
-if int(port) not in range(1,65535):
-     print '\nThat is an invalid port.\n\n'
+if int(port) not in range(1, 65535):
+	print '\nThat is an invalid port.\n\n'
 else:
-     print '\nThat is a valid port.\n\n'
-
+	print '\nThat is a valid port.\n\n'
--- misc/python/test.py
+++ misc/python/test.py
@@ -4,23 +4,22 @@ import os
 
 os.system('clear')
 
-f = open('tmp','r')                                # Setup a read connection to file
-filedata = f.read()                                # Read the file
-f.close()                                          # Close the connection
-filedata = filedata.split('\n')                    # Turn into a list
+f = open('tmp', 'r')  # Setup a read connection to file
+filedata = f.read()  # Read the file
+f.close()  # Close the connection
+filedata = filedata.split('\n')  # Turn into a list
 
 ##############################
 
-out = []                                           # Create an empty array
+out = []  # Create an empty array
 
 for i in filedata:
-     if '@' in i:                                  # grep '@'
-          if not 'apples' in i:                    # grep -v 'apples'
-               out.append(i.lower())               # Append to array and change to lower case
+	if '@' in i:  # grep '@'
+		if not 'apples' in i:  # grep -v 'apples'
+			out.append(i.lower())  # Append to array and change to lower case
 
-out = list(set(out))                               # Make list unique
-out.sort()                                         # Sort
+out = list(set(out))  # Make list unique
+out.sort()  # Sort
 
 for j in out:
-     print j
-
+	print j
